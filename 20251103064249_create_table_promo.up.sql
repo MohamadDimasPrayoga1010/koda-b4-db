1b9e1d2a6e696daafde9f752871e1390e48727f4
Mohamad Dimas Prayoga <mohamaddimasprayoga89@gmail.com>
2025-11-03 14:53:16 +0700

    refactor: fix typo

--- 20251103064249_create_table_promo.up.sql
+++ 20251103064249_create_table_promo.up.sql
@@ -4,14 +4,14 @@ CREATE TABLE promos (
     description VARCHAR(100) NOT NULL,
     discount FLOAT NOT NULL,
     start TIMESTAMP NOT NULL,
-    end TIMESTAMP NOT NULL,
+    "end" TIMESTAMP NOT NULL,
     created_at TIMESTAMP DEFAULT now(),
     updated_at TIMESTAMP DEFAULT now(),
     deleted_at TIMESTAMP
 );
 
 CREATE TABLE product_promos (
-    id SERIAL PRIMARY KEY
+    id SERIAL PRIMARY KEY,
     promo_id BIGINT REFERENCES promos(id),
     product_id BIGINT REFERENCES products(id)
 );
