e37d3a1a96d9cc6a1d76db93f0eee74de3811995
Mohamad Dimas Prayoga <mohamaddimasprayoga89@gmail.com>
2025-11-03 16:29:30 +0700

    reafctor: separate several tables and create new files to make it easier to update in the future

--- 20251103061606_create_table_products.down.sql
+++ 20251103061606_create_table_products.down.sql
@@ -1,6 +1,3 @@
-DROP TABLE IF EXISTS product_sizes;
-DROP TABLE IF EXISTS product_images;
-DROP TABLE IF EXISTS products_categories;
 DROP TABLE IF EXISTS products;
 DROP TABLE IF EXISTS categories;
 DROP TABLE IF EXISTS variants;
---
+++ 20251103092011_create_products_categories.up.sql
@@ -0,0 +1,17 @@
+CREATE TABLE products_categories(
+    product_id BIGINT REFERENCES products(id),
+    category_id BIGINT REFERENCES categories(id)
+);
+
+CREATE TABLE product_images(
+    id SERIAL PRIMARY KEY,
+    product_id BIGINT REFERENCES products(id),
+    image VARCHAR(250),
+    updated_at TIMESTAMP DEFAULT now(),
+    deleted_at TIMESTAMP DEFAULT now()
+);
+
+CREATE TABLE product_sizes(
+    product_id BIGINT REFERENCES products(id),
+    size_id INT REFERENCES sizes(id)
+)
