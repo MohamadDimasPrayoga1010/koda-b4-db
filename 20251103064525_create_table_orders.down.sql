e594a450b8e9e00c7d2e2a23e47da4bdf786967a
Mohamad Dimas Prayoga <mohamaddimasprayoga89@gmail.com>
2025-11-03 16:06:53 +0700

    feat: correct deletion order

--- 20251103064525_create_table_orders.down.sql
+++ 20251103064525_create_table_orders.down.sql
@@ -1,9 +1,6 @@
-DROP TABLE IF EXISTS payment_methods;
-
-DROP TABLE IF EXISTS orders;
-
 DROP TABLE IF EXISTS orders_products;
-
-DROP TABLE IF EXISTS shippings;
-
+DROP TABLE IF EXISTS orders;
 DROP TABLE IF EXISTS status;
+DROP TABLE IF EXISTS shippings;
+DROP TABLE IF EXISTS sizes;
+DROP TABLE IF EXISTS payment_methods;
