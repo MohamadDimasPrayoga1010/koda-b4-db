f7e556172d6ad075781197e5f3f324ec0092f95a
Mohamad Dimas Prayoga <mohamaddimasprayoga89@gmail.com>
2025-11-03 17:20:55 +0700

    feat: create products table for migration

---
+++ migrations/000005_create_products_table.up.sql
@@ -0,0 +1,12 @@
+CREATE TABLE products(
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(100),
+    description VARCHAR(250),
+    stock INT,
+    category_id BIGINT REFERENCES categories(id),
+    variant_id BIGINT REFERENCES variants(id),
+    base_price NUMERIC,
+    created_at TIMESTAMP DEFAULT now(),
+    updated_at TIMESTAMP DEFAULT now(),
+    deleted_at TIMESTAMP DEFAULT now()
+);
