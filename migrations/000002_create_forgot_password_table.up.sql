1109ab2cc7e35f4c45ca33b670222e723ff5ba4c
Mohamad Dimas Prayoga <mohamaddimasprayoga89@gmail.com>
2025-11-03 17:19:08 +0700

    feat: create forgot password table for migration

---
+++ migrations/000002_create_forgot_password_table.up.sql
@@ -0,0 +1,7 @@
+CREATE TABLE forgot_password(
+    id SERIAL PRIMARY KEY,
+    user_id BIGINT UNIQUE REFERENCES users(id),
+    token VARCHAR(100),
+    expires_at TIMESTAMP DEFAULT now(),
+    created_at TIMESTAMP DEFAULT now()
+);
