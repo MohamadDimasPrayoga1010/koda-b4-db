14a6b1a8823de73222bda3c352b8fef55cad8739
Mohamad Dimas Prayoga <mohamaddimasprayoga89@gmail.com>
2025-11-03 14:00:40 +0700

    feat: create table products for migration

---
+++ 20251103061606_create_table_products.down.sql
@@ -0,0 +1,6 @@
+DROP TABLE IF EXISTS product_sizes;
+DROP TABLE IF EXISTS product_images;
+DROP TABLE IF EXISTS products_categories;
+DROP TABLE IF EXISTS products;
+DROP TABLE IF EXISTS categories;
+DROP TABLE IF EXISTS variants;
